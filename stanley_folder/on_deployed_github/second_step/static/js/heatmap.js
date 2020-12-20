dcc1dc79802f95bd99d01bf558e60dcb35e3dc11
Stanley Tan <stanley.tan19@gmail.com>
2020-12-19 21:36:03 -0800

    deployed

---
+++ stanley_folder/on_deployed_github/second_step/static/js/heatmap.js
@@ -0,0 +1,189 @@
+var restaurants = [{'name': 'Man vs Fries',
+  'rating': 4.0,
+  'review_count': 39,
+  'coordinates_list': [37.7474, -122.41794],
+  'categories_list': ['Mexican', 'Pop-Up Restaurants'],
+  'url': 'https://www.yelp.com/biz/man-vs-fries-san-francisco-2?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'El Farolito',
+  'rating': 4.0,
+  'review_count': 4970,
+  'coordinates_list': [37.75265, -122.41812],
+  'categories_list': ['Mexican'],
+  'url': 'https://www.yelp.com/biz/el-farolito-san-francisco-2?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Tacos El Patrón',
+  'rating': 4.0,
+  'review_count': 409,
+  'coordinates_list': [37.748949, -122.416138],
+  'categories_list': ['Tacos'],
+  'url': 'https://www.yelp.com/biz/tacos-el-patr%C3%B3n-san-francisco-2?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'RakiRaki by Junya Watanabe',
+  'rating': 4.5,
+  'review_count': 11,
+  'coordinates_list': [37.7444, -122.420922],
+  'categories_list': ['Ramen', 'Sushi Bars'],
+  'url': 'https://www.yelp.com/biz/rakiraki-by-junya-watanabe-san-francisco-3?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Basa Seafood Express',
+  'rating': 4.5,
+  'review_count': 713,
+  'coordinates_list': [37.75276, -122.41338],
+  'categories_list': ['Seafood', 'Sandwiches', 'Burgers'],
+  'url': 'https://www.yelp.com/biz/basa-seafood-express-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Phat Philly',
+  'rating': 4.0,
+  'review_count': 1036,
+  'coordinates_list': [37.75224, -122.42037],
+  'categories_list': ['Sandwiches', 'Cheesesteaks', 'Chicken Wings'],
+  'url': 'https://www.yelp.com/biz/phat-philly-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Olinda Brazilian Kitchen',
+  'rating': 5.0,
+  'review_count': 1,
+  'coordinates_list': [37.7368916583, -122.405298],
+  'categories_list': ['Brazilian'],
+  'url': 'https://www.yelp.com/biz/olinda-brazilian-kitchen-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Chicken as Cluck',
+  'rating': 4.0,
+  'review_count': 153,
+  'coordinates_list': [37.7505862896473, -122.395178442908],
+  'categories_list': ['Chicken Shop', 'Food Delivery Services'],
+  'url': 'https://www.yelp.com/biz/chicken-as-cluck-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'United Dumplings',
+  'rating': 4.5,
+  'review_count': 53,
+  'coordinates_list': [37.739223, -122.416008],
+  'categories_list': ['Chinese', 'Asian Fusion', 'Noodles'],
+  'url': 'https://www.yelp.com/biz/united-dumplings-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Kazan',
+  'rating': 4.5,
+  'review_count': 440,
+  'coordinates_list': [37.752694, -122.408469],
+  'categories_list': ['Japanese', 'Sushi Bars', 'Soup'],
+  'url': 'https://www.yelp.com/biz/kazan-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Taqueria Vallarta',
+  'rating': 4.0,
+  'review_count': 819,
+  'coordinates_list': [37.7524796182579, -122.412584026201],
+  'categories_list': ['Tacos'],
+  'url': 'https://www.yelp.com/biz/taqueria-vallarta-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Project Boba',
+  'rating': 5.0,
+  'review_count': 16,
+  'coordinates_list': [37.736819, -122.405298],
+  'categories_list': ['Bubble Tea'],
+  'url': 'https://www.yelp.com/biz/project-boba-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Peaches Patties',
+  'rating': 4.5,
+  'review_count': 64,
+  'coordinates_list': [37.750142, -122.414113],
+  'categories_list': ['Caribbean'],
+  'url': 'https://www.yelp.com/biz/peaches-patties-san-francisco-2?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'The Front Porch',
+  'rating': 4.0,
+  'review_count': 2875,
+  'coordinates_list': [37.7438, -122.422035],
+  'categories_list': ['Southern', 'Cajun/Creole', 'American (Traditional)'],
+  'url': 'https://www.yelp.com/biz/the-front-porch-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'The Halal Guys',
+  'rating': 3.5,
+  'review_count': 840,
+  'coordinates_list': [37.786352, -122.4104376],
+  'categories_list': ['Halal', 'Middle Eastern', 'Mediterranean'],
+  'url': 'https://www.yelp.com/biz/the-halal-guys-san-francisco-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Top Round Roast Beef',
+  'rating': 4.0,
+  'review_count': 175,
+  'coordinates_list': [37.75289, -122.41139],
+  'categories_list': ['Fast Food', 'Sandwiches', 'Ice Cream & Frozen Yogurt'],
+  'url': 'https://www.yelp.com/biz/top-round-roast-beef-san-francisco-3?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Bac Lieu Restaurant',
+  'rating': 4.5,
+  'review_count': 198,
+  'coordinates_list': [37.745267, -122.420402],
+  'categories_list': ['Vietnamese', 'Noodles', 'Soup'],
+  'url': 'https://www.yelp.com/biz/bac-lieu-restaurant-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Yamo',
+  'rating': 4.0,
+  'review_count': 1988,
+  'coordinates_list': [37.7619463343878, -122.419764563536],
+  'categories_list': ['Burmese', 'Chinese'],
+  'url': 'https://www.yelp.com/biz/yamo-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Señor Sisig',
+  'rating': 4.0,
+  'review_count': 192,
+  'coordinates_list': [37.757154, -122.421326],
+  'categories_list': ['Filipino'],
+  'url': 'https://www.yelp.com/biz/se%C3%B1or-sisig-san-francisco-9?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'},
+ {'name': 'Farmhouse Kitchen Thai Cuisine',
+  'rating': 4.0,
+  'review_count': 2648,
+  'coordinates_list': [37.76026094, -122.4112048],
+  'categories_list': ['Thai', 'Comfort Food'],
+  'url': 'https://www.yelp.com/biz/farmhouse-kitchen-thai-cuisine-san-francisco?adjust_creative=Ew22WtqM03RGdHlepBFF7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Ew22WtqM03RGdHlepBFF7g'}]
+
+var restaurantMarkers = [];
+
+// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+for (var i = 0; i < restaurants.length; i++) {
+  restaurantMarkers.push(
+  L.marker(restaurants[i].coordinates_list)
+    .bindPopup("<h3>"+"<a href=\""+ restaurants[i].url + "\""+" target=\"_blank\">" + restaurants[i].name + "</a> </h3> <hr> <h5>Rating " + restaurants[i].rating + "</h5> <h5>Review Count: " + restaurants[i].review_count + "</h5> <h5>Categories: " + restaurants[i].categories_list + "</h5>")
+  ); 
+}
+
+var restaurantLayer = L.layerGroup(restaurantMarkers);
+
+//from 1.3.1_Markers
+
+
+var light = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+});
+
+var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
+
+var test_json = []
+
+d3.json(url, function(response) {
+
+  console.log(response);
+
+  test_json = response;
+  
+  var heatArray = [];
+
+  for (var i = 0; i < response.length; i++) {
+    var location = response[i].location;
+
+    if (location) {
+      heatArray.push([location.coordinates[1], location.coordinates[0]]);
+    }
+  }
+
+  var baseMaps = {
+    Light: light
+  }
+
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  });
+
+  var overlayMaps = {
+    Heat: heat,
+    Restaurant: restaurantLayer
+  }
+
+  var myMap = L.map("map", {
+    center: [37.7749, -122.4194],
+    zoom: 13,
+    layers: [light]
+  });
+  
+
+    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+});
