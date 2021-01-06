65af91ebef13716ec629a84f819c4cff052d6184
Jeremy Jang <jj2798@nyu.edu>
2021-01-05 18:01:03 -0800

    able to plot NYC weather data using chartjs, going to make minor adjustments to make the chart prettier

--- jeremy_folder/weather_and_movie/static/js/logic.js
+++ jeremy_folder/weather_and_movie/static/js/logic.js
@@ -1,36 +1,45 @@
-d3.csv("/static/data/nyc_weather_5_years.csv").then(generateChart);
 
-function generateChart(weather){
+// Step 1: Load the csv file
+test_data = []
 
+d3.csv('static/data/nyc_weather_5_years.csv')
+  .then(makeChart);
+
+function makeChart(weather) {
+  console.log(weather)
+  test_data = weather
+  
   var date = weather.map(function(d){return d.DATE;});
   var precipitation = weather.map(function(d){return d.PRCP;});
   var snow = weather.map(function(d){return d.SNOW;});
   var avgTemp = weather.map(function(d){return d.TAVG;});
 
-  var lineChart = new Chart(document.getElementById("lineChart"),{
-    type:'line',
-    data:{
-      labels: date,
-      datasets:[{
-          data: precipitation,
-          label: "Precipitation levels"
-        },
-        {
-          data: snow,
-          label: "Snow Levels"
-        },
-        {
-          data: avgTemp,
-          label: "Average Temperature (in Fahrenheit)"
-        },
-      ]
+
+var chart = new Chart('lineChart', {
+  type: 'line',
+  data: {
+    labels: date,
+    datasets:[{
+      data: precipitation,
+      label: "Precipitation levels"
     },
-    options: {
-      title: {
-        display: true,
-        text: "Weather in New York City from the last 5 years"
-      }
+    {
+      data: snow,
+      label: "Snow Levels"
+    },
+    {
+      data: avgTemp,
+      label: "Average Temperature (in Fahrenheit)"
     }
-  });
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'New York Temperature From the past 5 years'
+    }
+  }
+});
+
 
 }
